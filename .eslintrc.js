14b090174ab3c684064b8d38fc599fed73408a1c
DarkArtek <darkartek@gmail.com>
2022-09-11 07:08:31 +0200

    New Version

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,38 @@
-// @ts-check
-
-/** @type import('eslint').Linter.BaseConfig */
 module.exports = {
-  root: true,
-  extends: [
-    // basic
-    './index.js',
-  ],
-  env: {
-    node: true,
-  },
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended'
+	],
+	'overrides': [
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'@typescript-eslint'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
 };
